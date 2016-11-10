04fb3a969d12a15662fc3943ba2fea2de66a95d8
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-10 15:37:05 -0500

    Generate reservation model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110202139) do
+ActiveRecord::Schema.define(version: 20161110203535) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.date     "date"
+    t.time     "time"
+    t.integer  "party_size"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
