ca5c220884182a50f63957e06595b77af8545e8e
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-10 15:28:47 -0500

    Generate user model and migration with has_secure_password method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110200327) do
+ActiveRecord::Schema.define(version: 20161110202139) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20161110200327) do
     t.string   "cuisine_type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
