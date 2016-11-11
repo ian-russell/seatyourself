83361793ef59072b9404835abcc904174de8ccb6
Michael Schwartze <mschwartze@gmail.com>
2016-11-11 18:30:43 -0500

    Add current user method to Application Controller, add sign in details to application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroys]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :restaurants
   resources :users, only: [:new, :create]
 
