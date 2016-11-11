24b74b10e5d6b8567c675754bd5c8f2ceb1ef419
Michael Schwartze <mschwartze@gmail.com>
2016-11-11 15:07:15 -0500

    Generate user controller and route actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  resources :restaurants
+  resources :users, only: [:new, :create]
+
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
   # get 'restaurants/index'
   # get 'restaurants/show'
   # get 'restaurants/new'
   # get 'restaurants/edit'
-
-  resources :restaurants
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
