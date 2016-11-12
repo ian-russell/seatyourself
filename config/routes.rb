a43656dee252bb339dabc6a13775e951d7507994
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-12 15:51:11 -0500

    create partial for editing user account, added route, still need to update controller and fix view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit]
 
   resources :restaurants do
     resources :reservations, only: [:new, :show, :create, :destroy]
