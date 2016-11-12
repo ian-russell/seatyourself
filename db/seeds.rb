b2f25fa0c308e137729928b2f5f194d456b1f9cc
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-12 13:22:47 -0500

    adjusted index page for restaurants. Fixing show page for indicidual restaurant. Seeded database with more rest.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,31 @@ Restaurant.create(
   phone_number: "416-413-2222",
   cuisine_type: "Japanese"
 )
+
+Restaurant.create(
+  name: "Terroni",
+  address: "565 Terroni St.",
+  phone_number: "416-454-4545",
+  cuisine_type: "Italian"
+)
+
+Restaurant.create(
+  name: "Tom Jones Steakhouse",
+  address: "565 Tom Jones St.",
+  phone_number: "416-667-4648",
+  cuisine_type: "Steakhouse"
+)
+Restaurant.create(
+  name: "Woodlot",
+  address: "565 Woodlot St.",
+  phone_number: "416-444-4444",
+  cuisine_type: "Canadian"
+)
+
+
 Reservation.create(
   # date: "2016-11-25",
-  date: Date.new(2016, 11, 25), 
+  date: Date.new(2016, 11, 25),
   time: "18:00:00",
   party_size: 2,
   user_id: 1,
