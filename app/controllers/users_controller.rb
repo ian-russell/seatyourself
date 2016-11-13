43d6911c4896c9e78145ad947809bff422eb47f5
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-13 16:04:53 -0500

    update users controller - rediect to new session path once signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to restaurants_url
+      redirect_to new_session_path, notice: "Signed up!"
     else
       render "new"
     end
