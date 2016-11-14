4f15038343567f7f4173e82155223f5fe590d8f0
Michael Schwartze <mschwartze@gmail.com>
2016-11-14 15:18:36 -0500

    Add migration to add capacity to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110203535) do
+ActiveRecord::Schema.define(version: 20161114201606) do
 
   create_table "reservations", force: :cascade do |t|
     t.date     "date"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20161110203535) do
     t.string   "cuisine_type"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "capacity"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,6 +37,8 @@ ActiveRecord::Schema.define(version: 20161110203535) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
 end
