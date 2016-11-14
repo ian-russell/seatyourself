feccdf290af6a3c7d807aba984e613407a72dc03
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-13 19:10:37 -0500

    Disoplaying errors for creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reservations
   has_many :restaurants, through: :reservations
+
   validates :email, :password, :password_confirmation, presence: true
+
 end
