6cc080589ddac9ef40b41a989e51671de02f9678
Ian Russell <ian.andrew.russell@gmail.com>
2016-11-13 19:56:18 -0500

    reservations validators added. Reservation not displaying errors, just reloading reservations page

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
+  validates :date, :time, presence: true
+  validates :party_size, numericality: {only_integer: true}, presence: true
 
 end
