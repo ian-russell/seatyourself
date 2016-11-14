cbbe5124e8a024379354b9613db5ab8e72ca529f
Michael Schwartze <mschwartze@gmail.com>
2016-11-14 16:56:08 -0500

    Add date should be in the future validator to reservation.rb

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,13 @@ class Reservation < ApplicationRecord
   belongs_to :restaurant
 
   validates :date, :time, presence: true
+  validate :date_cannot_be_in_the_past
   validates :party_size, numericality: {only_integer: true}, presence: true
+
+  def date_cannot_be_in_the_past
+    if date.present? && date <= Date.today
+      errors.add(:date, "Can't be in the past")
+    end
+  end
 
 end
