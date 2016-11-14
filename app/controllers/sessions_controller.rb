b792ad4219a35c9e1be560150e1a5ad4caea1e89
Michael Schwartze <mschwartze@gmail.com>
2016-11-13 21:31:56 -0500

    Add expand and collapse view of restaurants by cuisine type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to restaurants_url
+      redirect_to restaurants_path
     else
       flash.now[:alert] = "Invalid email or password"
       render "new"
