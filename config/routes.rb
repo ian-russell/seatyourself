933f3d69fa6eae1c4b6728ab1465872d432a2ea8
Michael Schwartze <mschwartze@gmail.com>
2016-11-15 08:44:52 -0500

    Add root redirect to /restaurants in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => redirect('/restaurants')
+
   resources :restaurants
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show, :edit, :update]
