19706fa59c39374264f8ac11f632f6d72bf70c4d
Michael Schwartze <mschwartze@gmail.com>
2016-11-15 14:27:01 -0500

    Add validator to restrict reservations when restaurant is full at time and date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,11 +4,18 @@ class Reservation < ApplicationRecord
 
   validates :date, :time, presence: true
   validate :date_cannot_be_in_the_past
+  validate :restaurant_cannot_be_full
   validates :party_size, numericality: {only_integer: true}, presence: true
 
   def date_cannot_be_in_the_past
     if date.present? && date < Date.today
       errors.add(:date, "Can't be in the past")
+    end
+  end
+
+  def restaurant_cannot_be_full
+    unless restaurant.available?(party_size, time)
+      errors[:base] << "This restaurant is full at this time!"
     end
   end
 
