0bddb9f2eb6aee97e5e7a84fd686a8bbb2ec7cc4
Michael Schwartze <mschwartze@gmail.com>
2016-11-14 21:11:58 -0500

    Add methods to restaurant for checking capacity

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,20 @@
 class Restaurant < ApplicationRecord
   has_many :reservations
   has_many :users, through: :reservations
+
+  def available?(party_size, time)
+    party_size <= available_capacity(time)
+  end
+
+  private
+
+  def available_capacity(time)
+    capacity - reservations_at(time).sum(:party_size)
+  end
+
+  def reservations_at(time)
+    time = time.to_time
+    reservations.where('time >= ? AND time < ?', time.beginning_of_hour, time.end_of_hour)
+  end
+
 end
