ba2e886cb7a6b25aee31c85941ee9f79b0fab5ab
Michael Schwartze <mschwartze@gmail.com>
2016-11-15 13:50:37 -0500

    Add email format validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :restaurants, through: :reservations
 
   validates :email, :password, :firstname, :lastname, :password_confirmation, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :update }
   validates :email, uniqueness: { case_sensitive: false }
 
   def user_full_name
